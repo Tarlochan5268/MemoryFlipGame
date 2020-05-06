54a030dda5e916825872dc53a3185c0f6b94f437
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-05-06 06:39:05 -0400

    added array list of cards added shuffle function to easy level

--- app/src/main/java/com/tarlochan/memoryflipgame/EasyLevel.kt
+++ app/src/main/java/com/tarlochan/memoryflipgame/EasyLevel.kt
@@ -1,12 +1,55 @@
 package com.tarlochan.memoryflipgame
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.wajahatkarim3.easyflipview.EasyFlipView
+import java.util.*
+import kotlin.collections.ArrayList
 
 class EasyLevel : AppCompatActivity() {
+
+    private var EasyLevelRecyclerView: RecyclerView? = null
+    var cards: ArrayList<Int>? = null
+    var CARDS = intArrayOf(
+        R.drawable.card1,
+        R.drawable.card2,
+        R.drawable.card3,
+        R.drawable.card4,
+        R.drawable.card5,
+        R.drawable.card6,
+        R.drawable.card1,
+        R.drawable.card2,
+        R.drawable.card3,
+        R.drawable.card4,
+        R.drawable.card5,
+        R.drawable.card6
+    )
+    var flippedCard: EasyFlipView? = null
+    var RemainingTime: Long = 0
+    var isPaused = false
+    var isCancelled:Boolean = false
+    var b: Bundle? = null
+    private var pref: SharedPreferences? = null
+    var pos = 0
+    var count:Int = 0
+    var bestScore:Int = 0
+
+    fun shuffle(cards: IntArray, n: Int) {
+        val random = Random()
+        for (i in 0 until n) {
+            val r = random.nextInt(n - i)
+            val temp = cards[r]
+            cards[r] = cards[i]
+            cards[i] = temp
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_easy_level)
+        
+
     }
 }
--- app/src/main/java/com/tarlochan/memoryflipgame/HardLevel.kt
+++ app/src/main/java/com/tarlochan/memoryflipgame/HardLevel.kt
@@ -10,3 +10,43 @@ class HardLevel : AppCompatActivity() {
         setContentView(R.layout.activity_hard_level)
     }
 }
+/*
+
+private var EasyLevelRecyclerView: RecyclerView? = null
+    var cards: ArrayList<Int>? = null
+    var CARDS = intArrayOf(
+        R.drawable.card1,
+        R.drawable.card2,
+        R.drawable.card3,
+        R.drawable.card4,
+        R.drawable.card5,
+        R.drawable.card6,
+        R.drawable.card1,
+        R.drawable.card2,
+        R.drawable.card3,
+        R.drawable.card4,
+        R.drawable.card5,
+        R.drawable.card6
+    )
+    var flippedCard: EasyFlipView? = null
+    var RemainingTime: Long = 0
+    var isPaused = false, var isCancelled:kotlin.Boolean = false
+    var b: Bundle? = null
+    private var pref: SharedPreferences? = null
+    var pos = 0, var count:Int = 0, var bestScore:Int = 0
+
+
+    fun EasyLevel() {
+        // Required empty public constructor
+    }
+
+    fun shuffle(cards: IntArray, n: Int) {
+        val random = Random()
+        for (i in 0 until n) {
+            val r = random.nextInt(n - i)
+            val temp = cards[r]
+            cards[r] = cards[i]
+            cards[i] = temp
+        }
+    }
+ */
