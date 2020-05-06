ceaa8845cebcb304dc29931b5fdda20673f571e1
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-05-06 06:28:27 -0400

    Initial commit

---
+++ app/src/main/java/com/tarlochan/memoryflipgame/HardLevel.kt
@@ -0,0 +1,12 @@
+package com.tarlochan.memoryflipgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HardLevel : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hard_level)
+    }
+}
