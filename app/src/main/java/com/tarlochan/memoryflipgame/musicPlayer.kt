c45b548fb24cb4660add0e2de7344c13e760a1ba
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-05-07 12:43:24 -0400

    added Music Player class to play music when player win match

---
+++ app/src/main/java/com/tarlochan/memoryflipgame/musicPlayer.kt
@@ -0,0 +1,21 @@
+package com.tarlochan.memoryflipgame
+
+import android.content.Context
+import android.media.MediaPlayer
+
+class MusicPlayer(var ctx: Context?) {
+    var player: MediaPlayer? = null
+    fun playSound(fileName: String?) {
+        player = MediaPlayer()
+        try {
+            val afd = ctx!!.assets.openFd(fileName!!)
+            player!!.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+            afd.close()
+            player!!.prepare()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        player!!.start()
+    }
+
+}
