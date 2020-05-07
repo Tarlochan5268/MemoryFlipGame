d1cec28632d844044e98a44f507bd3ed4ff5651c
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-05-07 13:27:24 -0400

    Constants class modified

--- app/src/main/java/com/tarlochan/memoryflipgame/Constants.java
+++ app/src/main/java/com/tarlochan/memoryflipgame/Constants.java
@@ -11,8 +11,8 @@ public final class Constants {
     public static final int SUPER_HARD_NO_OF_CARDS = 20;
     public static final int HARD_NO_OF_CARDS = 16;
     public static final int EASY_NO_OF_CARDS = 12;
-    public static final long EASY_TIME = 22000;
-    public static final long HARD_TIME = 32000;
-    public static final long SUPER_HARD_TIME = 42000;
+    public static final long EASY_TIME = 32000;
+    public static final long HARD_TIME = 42000;
+    public static final long SUPER_HARD_TIME = 52000;
     public static final int TIMER_INTERVAL = 1000;
 }
