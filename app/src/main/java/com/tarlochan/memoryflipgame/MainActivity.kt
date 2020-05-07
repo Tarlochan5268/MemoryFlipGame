c734a7db234c303d5c10127b80f0cd3ee308b675
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-05-07 05:20:03 -0400

    modified Main Activity and its layout to handle fragments

--- app/src/main/java/com/tarlochan/memoryflipgame/MainActivity.kt
+++ app/src/main/java/com/tarlochan/memoryflipgame/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.tarlochan.memoryflipgame
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.layoutFragment, Home())
+        transaction.commit()
     }
 }
